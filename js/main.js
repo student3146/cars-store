9f70cb720f92cc03ba82e5017741e26b8eb41628
student3146 <iskostiuk3146@gmail.com>
2023-09-05 04:03:01 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+    const tabsItem = document.querySelectorAll('.tabs__btn-item');
+    const tabContent = document.querySelectorAll('.tabs__content-item');
+
+    tabsItem.forEach(function(el) {
+        el.addEventListener('click', open);
+    });
+
+    function open(event) {
+        const tabTarget = event.currentTarget;
+        const button = tabTarget.dataset.button;
+
+        tabsItem.forEach(function(item) {
+            item.classList.remove('tabs__btn-item--active');
+        });
+
+        tabTarget.classList.add('tabs__btn-item--active');
+
+        tabContent.forEach(function(item) {
+            item.classList.remove('tabs__content-item--active');
+        });
+
+        document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+    }
+
+
+    
+    const swiper = new Swiper('.swiper', {
+        effect: 'fade',
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
